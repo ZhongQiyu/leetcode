fe19cf8a727c8c4ddce211448d5c7670062b33fb
ZhongQiyu <zhongq@union.edu>
2021-10-24 16:49:37 -0400

    update 102421

--- LeetCodeTest.java
+++ LeetCodeTest.java
@@ -1,4 +1,12 @@
 public class LeetCodeTest {
+    
+    // credit to my fellow students' recommendations
+    
+    private int problemsSolved = 0;
+    private double timeAvg = 0.0;
+    private String todayDate = "";
+    private ArrayList<String> readTutorials = new ArrayList<>();
+    
     public static void main(String[] args){
         System.out.println("Welcome to the world of LeetCode");
     }
