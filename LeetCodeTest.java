80d382580bef6438e4b6d43f96a34f9b62ad0fc2
ZhongQiyu <zhongq@union.edu>
2021-11-28 17:48:06 -0500

    update 112821
    两数之和

--- LeetCodeTest.java
+++ LeetCodeTest.java
@@ -7,6 +7,28 @@ public class LeetCodeTest {
     private String todayDate = "";
     private ArrayList<String> readTutorials = new ArrayList<>();
     
+    public int[] twoSum(int[] nums, int target) {
+        int numCount = nums.length;
+        if(numCount < 2) {
+            return null;
+        }
+        else {
+            int[] indices = new int[2];
+            for(int i = 0; i < numCount - 1; i++) {
+                int first = nums[i];
+                for(int j = i + 1; j < numCount; j++) {
+                    int second = nums[j];
+                    if(first + second == target) {
+                        indices[0] = i;
+                        indices[1] = j;
+                        return indices;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+    
     public static void main(String[] args){
         System.out.println("Welcome to the world of LeetCode");
     }
